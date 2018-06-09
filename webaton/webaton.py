b89775c7e5e05611be83404820abf4c340fbeac2
nateshmbhat <nateshmbhat1@gmail.com>
2018-06-09 16:27:34 +0530

    find element method done[untested]

--- webaton/webaton.py
+++ webaton/webaton.py
@@ -61,20 +61,44 @@ class Browser:
 
             for element in text_matches_elements:
                 if(element.id in id_sets):
+                    continue
                     ''' No need to call the max method if the method call is ordered from most specific to least specific which naturally has the max score if the element is already present '''
-                    self.element_to_score[element] = max(self.element_to_score[element] , score ) ; 
+                    # self.element_to_score[element] = max(self.element_to_score[element] , score ) ; 
                 else:
                     self.element_to_score[element] = score ; 
-                
+
+        
+        def add_to_init_text_matches_score_from_xpath(xpath , score ):
+            add_to_init_text_matches_score( xpath , score ) ;
 
 
 
-        add_to_init_text_matches_score(self.driver.find_elements_by_xpath("//body/*[text()='{}']".format(text)) , score=15 ) ;
-        add_to_init_text_matches_score(self.driver.find_elements_by_link_text("{}".format(text)) , score=13 ) ;
 
-        add_to_init_text_matches_score(self.driver.find_elements_by_xpath("//body/*[contains(text() , {})]".format(text)) , score=10)
+        add_to_init_text_matches_score_from_xpath(("//body//input[@value='{}']".format(text)) , score=45 )
 
-        add_to_init_text_matches_score(self.driver.find_elements_by_xpath("//body/*[contains(translate(text() , {} , {} ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=7) ; 
+        add_to_init_text_matches_score_from_xpath(("//body//button[text()='{}']".format(text)) , score=45)
+
+
+        add_to_init_text_matches_score(self.driver.find_elements_by_link_text("{}".format(text)) , score=43 ) ;
+
+        add_to_init_text_matches_score_from_xpath(("//body//input[contains( @value , {})]".format(text)) , score=37 )
+
+        add_to_init_text_matches_score_from_xpath(("//body//button[contains(text() , {})]".format(text)) , score=37 )
+
+
+        add_to_init_text_matches_score_from_xpath(("//body//input[contains(translate( @value , {} , {} ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
+        add_to_init_text_matches_score_from_xpath(("//body//button[contains(translate(text() , {} , {} ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
+
+
+        add_to_init_text_matches_score_from_xpath("//body//*[@value='{}']".format(text) , score=30 ) ;
+        add_to_init_text_matches_score_from_xpath("//body//*[text()='{}']".format(text) , score=30 ) ;
+
+
+
+        add_to_init_text_matches_score_from_xpath(("//body//*[contains(text() , {})]".format(text)) , score=27 )
+
+        add_to_init_text_matches_score_from_xpath(("//body//*[contains(translate(text() , {} , {} ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=25) ; 
+                        
 
 
         for element in self.element_to_score.keys():
@@ -84,13 +108,31 @@ class Browser:
             if(id and id==element.get_attribute('id')):
                 score += 30
 
-            #Check Class > score+=20
-            if(classname and classname==element.get_attribute('class')):
-                score+=20 
+            #Check Class > score+=25
+            if(classname and classname in element.get_attribute('class').split()):
+                score+=25 
 
             #Check element tag and check for button or anchor  or input or textarea
+            if(type.lower() in ["button",'link'] and element.tag_name in ['button' , 'a'] or (type.lower()=='input' and 'input' == element.tag_name) ):
+                score+=20
             
-            
+            # If user doesn't enter any type
+            if(type is None and element.tag_name in ['button' , 'a' , 'input'] ):
+                score+=17
+
+        
+        max_score = max(self.element_to_score.values())
+        max_scored_elements = [element for element in self.element_to_score.keys() if (self.element_to_score[element]==max_score)]
+
+
+        if(len(max_scored_elements)):
+            return max_scored_elements[0] if (len(max_scored_elements)<number) else max_scored_elements[number]
+
+        print("No element found !") ; 
+
+        # sorted_element_scores_tup_list = sorted(self.element_to_score.items(), key=lambda tup: tup[1])
+
+           
 
 
 
@@ -105,6 +147,7 @@ class Browser:
 
     def click(self , text='' , type = "button" , tag='', id ='' , classname ='',  number = 1 , css_selector='' , xpath=''):
         element = self.__find_element__(text , type , tag , classname , id , number , css_selector , xpath) 
+        print(self.element_to_score)
 
 
     def write(self , into=None):
@@ -114,3 +157,9 @@ class Browser:
 
 class Chrome:
     pass
+
+
+if(__name__=='__main__'):
+    aton = Browser() ; 
+    aton.go_to('http://daedalcrafters.pythonanywhere.com') 
+    aton.click('Login' , type='button')
