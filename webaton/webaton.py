d5c22f2441a3ed7e6aa1628c345ad2a229032e68
nateshmbhat <nateshmbhat1@gmail.com>
2018-06-10 09:53:34 +0530

    added input filters for buttons and input

--- webaton/webaton.py
+++ webaton/webaton.py
@@ -1,4 +1,5 @@
 from selenium import webdriver ;
+from collections import OrderedDict
 from selenium.common import exceptions
 from time import sleep 
 import sys , argparse , os  
@@ -24,20 +25,22 @@ class Browser:
 
         def add_to_init_text_matches_score(text_matches_elements : list , score :int )->None:
             '''Extends a dictionary and maps it with the text_matched_element with the score'''
-            
 
             for element in text_matches_elements:
+
+                if(not element.is_displayed):
+                    continue ; 
+
                 if(element.id in self.element_to_score_id_set):
                     ''' No need to call the max method if the method call is ordered from most specific to least specific which naturally has the max score if the element is already present '''
                     self.element_to_score[element] = max(self.element_to_score[element] , score ) ; 
                 else:
                     self.element_to_score[element] = score ; 
                     self.element_to_score_id_set.add(element.id)
-
         
+
         def add_to_init_text_matches_score_from_xpath(xpath , score ):
             add_to_init_text_matches_score( self.driver.find_elements_by_xpath(xpath) , score ) ;
-
 
 
 
@@ -45,6 +48,9 @@ class Browser:
             '''This method finds the input tag elements by taking in the label elements and assigns the score argument to the new found input elements and puts them in the  elemenet to score mapping '''
 
             for element in elementlist:
+                if(not element.is_displayed):
+                    continue ; 
+
                 possible_input_id = element.get_attribute('for') ; 
                 try : 
                     add_to_init_text_matches_score_from_xpath(("//body//input[@id='{}']".format(possible_input_id)) , score )
@@ -76,33 +82,40 @@ class Browser:
             add_to_init_text_matches_score_from_xpath("//body//input[contains(translate(@name , 'USERNAME' , 'username' )  , 'username') or contains(translate(@name ,'EMAIL' , 'email' ) , 'email') or contains(translate(@name , 'LOGIN' , 'login'  ) , 'login' ) ]" , 53 )
 
 
-        add_to_init_text_matches_score_from_xpath(("//body//input[@value='{}']".format(text)) , score=45 )
-        add_to_init_text_matches_score_from_xpath(("//body//input[@placeholder='{}']".format(text)) , score=45 )
+        if(tag=='input'):
+            add_to_init_text_matches_score_from_xpath(("//body//input[@value='{}']".format(text)) , score=45 )
+            add_to_init_text_matches_score_from_xpath(("//body//input[@placeholder='{}']".format(text)) , score=45 )
 
-        find_input_element_for_label(self.driver.find_elements_by_xpath("//body//label[text()='{}']".format(text)) , score =45)
+            find_input_element_for_label(self.driver.find_elements_by_xpath("//body//label[text()='{}']".format(text)) , score =45)
 
+            add_to_init_text_matches_score_from_xpath(("//body//input[contains( @value , '{}')]".format(text)) , score=37 )
+            add_to_init_text_matches_score_from_xpath(("//body//input[contains( @placeholder , '{}')]".format(text)) , score=37 )
+            find_input_element_for_label(self.driver.find_elements_by_xpath("//body//label[contains( text() , '{}')]".format(text)) , score=37 )
 
-        add_to_init_text_matches_score_from_xpath(("//body//button[text()='{}']".format(text)) , score=45)
-
-
-        add_to_init_text_matches_score(self.driver.find_elements_by_link_text("{}".format(text)) , score=43 ) ;
-
-        add_to_init_text_matches_score_from_xpath(("//body//input[contains( @value , '{}')]".format(text)) , score=37 )
-        add_to_init_text_matches_score_from_xpath(("//body//input[contains( @placeholder , '{}')]".format(text)) , score=37 )
-        find_input_element_for_label(self.driver.find_elements_by_xpath("//body//label[contains( text() , '{}')]".format(text)) , score=37 )
+            add_to_init_text_matches_score_from_xpath(("//body//input[contains(translate( @value , '{}' , '{}' ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
+            add_to_init_text_matches_score_from_xpath(("//body//input[contains(translate( @placeholder , '{}' , '{}' ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
 
 
-        add_to_init_text_matches_score_from_xpath(("//body//button[contains(text() , '{}')]".format(text)) , score=37 )
+            find_input_element_for_label(self.driver.find_elements_by_xpath("//body//label[contains(translate( text() , '{}' , '{}' ) , '{}')]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
 
 
-        add_to_init_text_matches_score_from_xpath(("//body//input[contains(translate( @value , '{}' , '{}' ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
-        add_to_init_text_matches_score_from_xpath(("//body//input[contains(translate( @placeholder , '{}' , '{}' ) , {})]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
+
+        if(tag=='button'):
+
+            add_to_init_text_matches_score_from_xpath(("//body//button[text()='{}']".format(text)) , score=45)
+            add_to_init_text_matches_score_from_xpath(("//body//button//*[text()='{}']".format(text)) , score=45)
+
+            add_to_init_text_matches_score(self.driver.find_elements_by_link_text("{}".format(text)) , score=43 ) ;
+
+            add_to_init_text_matches_score_from_xpath(("//body//button[contains(text() , '{}')]".format(text)) , score=37 )
+            add_to_init_text_matches_score_from_xpath(("//body//button//*[contains(text() , '{}')]".format(text)) , score=37 )
+
+            add_to_init_text_matches_score_from_xpath(("//body//button[contains(translate(text() , '{}' , '{}' ) , '{}')]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
+            add_to_init_text_matches_score_from_xpath(("//body//button//*[contains(translate(text() , '{}' , '{}' ) , '{}')]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
 
 
-        find_input_element_for_label(self.driver.find_elements_by_xpath("//body//label[contains(translate( text() , '{}' , '{}' ) , '{}')]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
 
 
-        add_to_init_text_matches_score_from_xpath(("//body//button[contains(translate(text() , '{}' , '{}' ) , '{}')]".format(text.upper() , text.lower() , text.lower())) ,score=33) ; 
 
 
         if(match_level=='loose'):
@@ -119,25 +132,30 @@ class Browser:
         for element in self.element_to_score.keys():
             score = self.element_to_score.get(element) ; 
 
-            # Check ID > score+= 30
+            # Check ID
             if(id and id==element.get_attribute('id')):
-                score += 30
+                score += 100
 
-            #Check Class > score+=25
+            #Check Class
             if(classname and classname in element.get_attribute('class').split()):
-                score+=25 
+                score+=50
 
             #Check element tag and check for button or anchor  or input or textarea
             if(tag.lower() in ["button",'link'] and element.tag_name in ['button' , 'a'] or (tag.lower()=='input' and 'input' == element.tag_name) ):
-                score+=20
+                score+=35
             
-            # If user doesn't enter any tag
-            if(tag is None and element.tag_name in ['button' , 'a' , 'input'] ):
-                score+=17
+            # If user doesn't enter any tag [stick to default i.e button for click and input for type method ]
+            if(tag in ['button' , 'input'] and element.tag_name in ['button' , 'a' , 'input'] ):
+                score+=30
+
+            self.element_to_score[element] = score; 
         
 
         max_score = max(self.element_to_score.values())
         max_scored_elements = [element for element in self.element_to_score.keys() if (self.element_to_score[element]==max_score)]
+
+        self._max_score_elements_ = max_scored_elements 
+        self._max_score_ = max_score
 
         print("\n\nMax SCORES " , max_scored_elements) ; 
 
