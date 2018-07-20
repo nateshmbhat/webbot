39d29c73349561a4a71122e74c013e740078c2d5
nateshmbhat <nateshmbhat1@gmail.com>
2018-07-20 11:32:12 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst' , 'r' , encoding='utf-8') as f:
 setup(
     name='webbot',
     packages = ['webbot','webbot.drivers'] ,
-    version = '0.0.6',
+    version = '0.0.7',
     long_description = long_description , 
     install_requires = ['selenium'] , 
     package_data = {'' : [r'drivers/*']} , 
