3af2902366b13573cc2389760c1f365e19d32c9e
br0kE <167619285+br0k3V@users.noreply.github.com>
2024-04-26 09:21:01 +0800

    Update webbot.py (fix errors when using)

--- webbot/webbot.py
+++ webbot/webbot.py
@@ -24,9 +24,8 @@ class Browser:
         The constructor takes showWindow flag as argument which Defaults to False. If it is set to true , all browser happen without showing up any GUI window .
 
         :Args:
-            - showWindow : If false , will run a headless browser without showing GUI window.
+            - showWindow : If true , will run a headless browser without showing GUI window.
             - proxy : Url of any optional proxy server.
-            - driverPath: can specify the path of an alternative chromedriver
 
 
 
@@ -38,46 +37,36 @@ class Browser:
         - List containing all the errors which might have occurred during performing an action like click ,type etc.
     """
 
-    def __init__(self, showWindow=True, proxy=None , downloadPath:str=None, driverPath:str=None, arguments=["--disable-dev-shm-usage","--no-sandbox"]):
+    def __init__(self, showWindow=True, proxy=None , downloadPath:str=None):
         options = webdriver.ChromeOptions()
-
-        for argument in arguments:
-            options.add_argument(argument)
-
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
         if downloadPath is not None and isinstance(downloadPath,str):
             absolutePath = os.path.abspath(downloadPath)
             if(not os.path.isdir(absolutePath)):
                 raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), absolutePath)
-                
+
             options.add_experimental_option('prefs', {'download.default_directory' : absolutePath})
-            
-        if driverPath is not None and isinstance(driverPath,str):
-            driverPath = os.path.abspath(driverPath)
-            if(not os.path.isdir(driverPath)):
-                raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), driverPath)
 
         if proxy is not None and isinstance(proxy, str):
-            # Check if '--proxy-server' has not yet been set
-            if not any(arg.starts_with("--proxy-server") for arg in arguments):
-                options.add_argument("--proxy-server={}".format(proxy))
+            options.add_argument("--proxy-server={}".format(proxy))
 
-        if not showWindow and '--headless' not in arguments:
+        if not showWindow:
             options.headless = True
-            options.add_argument("--headless")
 
-        if driverPath is None:
-            driverfilename = ''
-            if sys.platform == 'linux' or sys.platform == 'linux2':
-                driverfilename = 'chrome_linux'
-            elif sys.platform == 'win32':
-                driverfilename = 'chrome_windows.exe'
-            elif sys.platform == 'darwin':
-                driverfilename = 'chrome_mac'
-            driverPath = os.path.join(os.path.split(__file__)[0], 'drivers{0}{1}'.format(os.path.sep, driverfilename))
+        driverfilename = ''
+        if sys.platform == 'linux' or sys.platform == 'linux2':
+            driverfilename = 'chrome_linux'
+        elif sys.platform == 'win32':
+            driverfilename = 'chrome_windows.exe'
+        elif sys.platform == 'darwin':
+            driverfilename = 'chrome_mac'
+        driverpath = os.path.join(os.path.split(__file__)[0], 'drivers{0}{1}'.format(os.path.sep, driverfilename))
 
-            os.chmod(driverPath, 0o755)
+        os.chmod(driverpath, 0o755)
+        
 
-        self.driver = webdriver.Chrome(executable_path=driverPath, options=options)
+        self.driver = webdriver.Chrome(keep_alive=True, options=options,)
         self.Key = Keys
         self.errors = []
 
@@ -87,7 +76,7 @@ class Browser:
           'get_screenshot_as_base64', 'get_screenshot_as_file', 'get_screenshot_as_png', 'get_window_position',
           'get_window_rect', 'get_window_size', 'maximize_window', 'minimize_window', 'implicitly_wait', 'quit',
           'refresh', 'save_screenshot', 'set_network_conditions', 'set_page_load_timeout', 'set_script_timeout',
-          'set_window_position', 'set_window_rect', 'start_client', 'start_session', 'stop_client', 'switch_to_alert']]
+          'set_window_position', 'set_window_rect', 'start_client', 'start_session', 'stop_client']]
 
     def close_current_tab(self):
         """Closes the current tab which the driver is controlling"""
